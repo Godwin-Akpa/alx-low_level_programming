4e71810aa03c926fd9d707770e9272267dfe954b
Godwin-Akpa <godwindevops@gmail.com>
2023-06-16 15:09:42 -0700

    Hello, put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+	/* this is to use the put function
+	 * in absence of printf 
+	 */
+int main(void)
+{
+	put("Programming is like building a multilingualpuzzle\n");
+
+	return (0);
+}
