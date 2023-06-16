b1205b67ed295ae6b71e7b932ccc5905308386bd
Godwin-Akpa <godwindevops@gmail.com>
2023-06-16 15:29:53 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Entry point = mains
+ *
+ * Return(0)+ success
+ */
+
+int main (void)
+
+{
+	printf("With proper grammar, but the outcome is a piece of art");
+
+	return(0);
+}
