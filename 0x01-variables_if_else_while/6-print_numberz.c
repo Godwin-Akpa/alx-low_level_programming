153226a568352df17da2b47f6a29db04b3d589aa
Godwin-Akpa <godwindevops@gmail.com>
2023-06-19 07:17:21 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery Point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
