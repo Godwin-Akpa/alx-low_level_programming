c456e2cdae8d19e484edda2e28042b3c73ae75dc
Godwin-Akpa <godwindevops@gmail.com>
2023-06-19 05:49:40 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
