506f0c1cabda4f9e1de8b584859e953589c72870
Godwin-Akpa <godwindevops@gmail.com>
2023-06-20 05:43:38 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *print_alphabet - a function that 
+ *prints 10 times the alphabet
+ *in lowercase, followed by a new line
+ *Return: 0 always success
+ */
+int print_alphabet_x10(void)
+{
+    int counter = 0;
+    char letter = 'a';
+
+    while (counter < 10) {
+        int i = 0;
+        while (i < 26) {
+            _putchar(letter);
+            letter++;
+            i++;
+        }
+
+        counter++;
+        letter = 'a';
+    }
+
+    _putchar('\n');
+}
